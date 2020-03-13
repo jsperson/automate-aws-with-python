dfb3ea11897babed528b9c2cd2f487b04f4ee434
jsperson <jsperson@gmail.com>
2020-03-13 14:19:31 -0400

    Changes from opening in VSCode

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -4,17 +4,20 @@ import click
 session = boto3.Session()
 s3 = session.resource('s3')
 
+
 @click.group()
 def cli():
     "Webotron deploys websites to AWS"
     pass
+
 
 @cli.command('list-buckets')
 def list_buckets():
     "List all s3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
-        
+
+
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
@@ -22,5 +25,7 @@ def list_bucket_objects(bucket):
     bucket = s3.Bucket(bucket)
     for obj in bucket.objects.all():
         print(obj)
+
+
 if __name__ == '__main__':
     cli()
